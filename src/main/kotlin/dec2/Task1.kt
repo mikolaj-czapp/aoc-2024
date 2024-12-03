278650fdc4e7c873432c78d4a358e81e5a04248e
Mikołaj Czapp <mikolaj.czapp@sparkbit.pl>
2024-12-03 17:36:57 +0100

    Refactor for both parts day 2

--- src/main/kotlin/dec2/Task1.kt
+++ src/main/kotlin/dec2/Task1.kt
@@ -1,81 +1,103 @@
 package org.example.dec2
 
 import org.example.InputReader
+import java.io.BufferedReader
 import kotlin.math.abs
 
 fun main() {
-    val inputReader = InputReader().getBufferedReader("dec2/input1.txt")
+    val inputReaderTask1 = InputReader().getBufferedReader("dec2/test1.txt")
+    println("Correct reports: ${Dec2.task1(inputReaderTask1)}")
 
-    var correctReports = 0
-    inputReader.lines().forEach {
-        val sanitized = sanitizeReport(it)
-        if (isReportCorrect(sanitized)) {
-            correctReports++
-        } else if (isDampenedReportCorrect(sanitized)) {
-            correctReports++
-        }
-    }
-    println(correctReports)
+    val inputReaderTask2 = InputReader().getBufferedReader("dec2/test1.txt")
+    println("Correct reports: ${Dec2.task2(inputReaderTask2)}")
 }
 
-private fun sanitizeReport(report: String): List<Int> = report.split(Regex("\\s+")).map { it.toInt() }
-
-private fun isReportCorrect(report: List<Int>): Boolean = isReportMonotonic(report) && isReportGradual(report, 1, 3)
-
-private fun isDampenedReportCorrect(report: List<Int>): Boolean {
-    report.forEachIndexed { index, i ->
-        if (isReportCorrect(dampReport(report, index))) {
-            return true
-        }
-    }
-    return false
-}
-
-private fun dampReport(
-    report: List<Int>,
-    position: Int,
-): List<Int> = report.subList(0, position) + report.subList(position + 1, report.size)
-
-private fun isReportMonotonic(report: List<Int>): Boolean {
-    val monotonicityContext = MonotonicityContext()
-    report.forEachIndexed { index, i ->
-        if ((index + 1) < report.size) {
-            if (report[index + 1] > i) {
-                monotonicityContext.setState(MonotonicityState.INCREASING)
+class Dec2 {
+    companion object {
+        fun task1(inputReader: BufferedReader): Int {
+            var correctReports = 0
+            inputReader.lines().forEach {
+                val sanitized = sanitizeReport(it)
+                if (isReportCorrect(sanitized)) {
+                    correctReports++
+                }
             }
-            if (report[index + 1] < i) {
-                monotonicityContext.setState(MonotonicityState.DECREASING)
+            return correctReports
+        }
+
+        fun task2(inputReader: BufferedReader): Int {
+            var correctReports = 0
+            inputReader.lines().forEach {
+                val sanitized = sanitizeReport(it)
+                if (isReportCorrect(sanitized)) {
+                    correctReports++
+                } else if (isDampenedReportCorrect(sanitized)) {
+                    correctReports++
+                }
             }
+            return correctReports
+        }
+
+        private enum class MonotonicityState {
+            INITIAL,
+            INCREASING,
+            DECREASING,
+        }
+
+        private fun sanitizeReport(report: String): List<Int> = report.split(Regex("\\s+")).map { it.toInt() }
+
+        private fun isReportCorrect(report: List<Int>): Boolean = isReportMonotonic(report) && isReportGradual(report, 1, 3)
+
+        private fun isDampenedReportCorrect(report: List<Int>): Boolean {
+            report.forEachIndexed { index, i ->
+                if (isReportCorrect(dampReport(report, index))) {
+                    return true
+                }
+            }
+            return false
+        }
+
+        private fun dampReport(
+            report: List<Int>,
+            position: Int,
+        ): List<Int> = report.subList(0, position) + report.subList(position + 1, report.size)
+
+        private fun isReportMonotonic(report: List<Int>): Boolean {
+            val monotonicityContext = MonotonicityContext()
+            report.forEachIndexed { index, i ->
+                if ((index + 1) < report.size) {
+                    if (report[index + 1] > i) {
+                        monotonicityContext.setState(MonotonicityState.INCREASING)
+                    }
+                    if (report[index + 1] < i) {
+                        monotonicityContext.setState(MonotonicityState.DECREASING)
+                    }
+                }
+            }
+            return monotonicityContext.getStateChangedCount() <= 1
+        }
+
+        private fun isReportGradual(
+            report: List<Int>,
+            atLeast: Int,
+            atMost: Int,
+        ): Boolean {
+            val sumOfDifferences = report.zipWithNext()
+            return sumOfDifferences.all { abs(it.first - it.second) >= atLeast && abs(it.second - it.first) <= atMost }
+        }
+
+        private class MonotonicityContext {
+            private var currentState: MonotonicityState = MonotonicityState.INITIAL
+            private var stateChangedCount = 0
+
+            fun setState(state: MonotonicityState) {
+                if (currentState != state) {
+                    stateChangedCount++
+                }
+                currentState = state
+            }
+
+            fun getStateChangedCount(): Int = stateChangedCount
         }
     }
-    return monotonicityContext.getStateChangedCount() <= 1
-}
-
-private fun isReportGradual(
-    report: List<Int>,
-    atLeast: Int,
-    atMost: Int,
-): Boolean {
-    val sumOfDifferences = report.zipWithNext()
-    return sumOfDifferences.all { abs(it.first - it.second) >= atLeast && abs(it.second - it.first) <= atMost }
-}
-
-private class MonotonicityContext {
-    private var currentState: MonotonicityState = MonotonicityState.INITIAL
-    private var stateChangedCount = 0
-
-    fun setState(state: MonotonicityState) {
-        if (currentState != state) {
-            stateChangedCount++
-        }
-        currentState = state
-    }
-
-    fun getStateChangedCount(): Int = stateChangedCount
-}
-
-private enum class MonotonicityState {
-    INITIAL,
-    INCREASING,
-    DECREASING,
 }
