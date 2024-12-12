62175c17c3478368e728bd949eee41038868300a
Mikołaj Czapp <mikolaj.czapp@gmail.com>
2024-12-12 20:42:57 +0100

    lint

--- src/main/kotlin/common/extensions.kt
+++ src/main/kotlin/common/extensions.kt
@@ -3,6 +3,7 @@ package org.example.common
 fun Pair<Int, Int>.times(multiplier: Int): Pair<Int, Int> = Pair(this.first * multiplier, this.second * multiplier)
 
 fun Pair<Int, Int>.add(other: Pair<Int, Int>) = Pair(this.first + other.first, this.second + other.second)
+
 fun Pair<Int, Int>.sub(other: Pair<Int, Int>) = Pair(this.first - other.first, this.second - other.second)
 
 fun List<List<Char>>.at(pair: Pair<Int, Int>): Char = this[pair.first][pair.second]
@@ -23,5 +24,4 @@ fun <A> Collection<A>.cartesianProduct(): List<Pair<A, A>> =
         this.map { it to a }
     }
 
-fun <A>  List<Pair<A, A>>.removeDiagonal(): List<Pair<A, A>> =
-    this.filter { it.first != it.second }
+fun <A> List<Pair<A, A>>.removeDiagonal(): List<Pair<A, A>> = this.filter { it.first != it.second }
